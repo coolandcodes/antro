95a173f4111356c8a3e148432947f8b34c43cceb
Ifeora Okechukwu Patrick <isocroft@users.noreply.github.com>
2026-08-23 00:37:44 +0100

    chore: refactored LexisException to extend RuntimeException
    Changed LexisException to extend RuntimeException instead of Exception.

--- src/com/codedev/antro/compiler/frontend/ast/contracts/concerns/LexisException.java
+++ src/com/codedev/antro/compiler/frontend/ast/contracts/concerns/LexisException.java
@@ -1,6 +1,12 @@
 package com.codedev.antro.compiler.frontend.contracts.concerns;
 
-public class LexisException extends Exception {
+/* @INFO: `RuntimeException` inheriting from `Exception` was a weird mistake! */
+/* @INFO: Also, both `RuntimeException` and `Exception` should have been abstract! */
+
+/**
+ * Custom unchecked exception to carry any checked exception exposed by the tokenizer
+ */
+public class LexisException extends RuntimeException {
     public LexisException(String message, Throwable cause) {
         super(message);
         this.initCause(cause); 
