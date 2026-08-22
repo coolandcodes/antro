135851d52336a11b40f6fa7ef5f4a0c9c8e7b0d5
Ifeora Okechukwu Patrick <isocroft@users.noreply.github.com>
2026-08-23 00:38:06 +0100

    chore: refactored UnexpectedEndOfInputException to extend RuntimeException
    Added comments clarifying the design decisions regarding RuntimeException and Exception.

--- src/com/codedev/antro/compiler/frontend/ast/contracts/concerns/UnexpectedEndOfInputException.java
+++ src/com/codedev/antro/compiler/frontend/ast/contracts/concerns/UnexpectedEndOfInputException.java
@@ -1,5 +1,13 @@
 package com.codedev.antro.compiler.frontend.contracts.concerns;
 
+/* @INFO: `RuntimeException` inheriting from `Exception` was a weird mistake! */
+/* @INFO: Also, both `RuntimeException` and `Exception` should have been abstract! */
+
+/**
+ * Custom unchecked exception to carry a checked exception exposed by the parser
+ * in the specific case where more tokens are expected yet the `EOF` token type is
+ * encountered unexpectedly.
+ */
 public class UnexpectedEndOfInputException extends RuntimeException {
     public UnexpectedEndOfInputException(String message, Throwable cause) {
         super(message);
